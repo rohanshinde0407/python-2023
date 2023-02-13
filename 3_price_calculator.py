44d6c4e5fb1a2aab81dcd3b454802088c26bc117
rohanshinde0407 <92221855+rohanshinde0407@users.noreply.github.com>
2023-02-13 22:51:08 +0530

    Add files via upload

---
+++ 3_price_calculator.py
@@ -0,0 +1,9 @@
+#this ia a python comment in my first python application 
+#PEP8
+quantity = 10
+#this variable contains a float
+unit_price = 25.25
+#this variable contains result of the multiplication
+extended_price = quantity * unit_price
+#show the result
+print("Rs",extended_price)
