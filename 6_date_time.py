7782f5fd227fb5e16fa83e13fe43ebca7f994b71
rohanshinde0407 <92221855+rohanshinde0407@users.noreply.github.com>
2023-02-13 23:08:38 +0530

    Add files via upload

---
+++ 6_date_time.py
@@ -0,0 +1,16 @@
+#import the datetime module , nickname dt
+import datetime as dt
+#show's today's date in a  variable 
+today = dt.date.today()
+print(today.month)
+#printing today info
+print(today.month)
+print(today.day)
+print(today.year)
+
+#store a data new year
+new_year = dt.dtodayate(2023,1,1)
+print(today.month)
+print(today.day)
+print(today.year)
+# days remaining
