fff472fe26504441627c9a85cd05dadbb05d30ca
rohanshinde0407 <92221855+rohanshinde0407@users.noreply.github.com>
2023-02-13 22:56:48 +0530

    Add files via upload

---
+++ 5_string.py
@@ -0,0 +1,8 @@
+#string manipulation
+prefix="Sir"
+first_name= "Ratan"
+last_name= "Tata"
+fullname= prefix + first_name +last_name
+
+fullname_withspaces= prefix + " " + first_name + " " + last_name
+print(fullname_withspaces)
